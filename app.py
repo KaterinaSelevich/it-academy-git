c40d8fd29eef1b6db703f69d3d2a07d754fe52de
KaterinaSelevich <katushaaa89@mail.ru>
2021-11-29 16:58:06 +0300

    commit message

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 print('I’m planning to learn git conflicts resolution')
-name = input('enter your name:')
-surname = input('Please enter your surname:')
-nickname = input('Please enter your nickname')
-print('Hi, {}!'.format(nickname))
+name = input(‘Please enter your name:’)
+surname = input(‘Please enter your surname:’)
+print(‘Hi, {} {}!’.format(name, surname))
