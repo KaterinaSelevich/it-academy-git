4a4e09e3e0d237714de6b78e663faf59e1301f8e
KaterinaSelevich <92476413+KaterinaSelevich@users.noreply.github.com>
2021-12-12 19:17:22 +0300

    Merge remote-tracking branch 'origin/Katerina_Selevich'

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 print('I’m planning to learn git conflicts resolution')
-<<<<<<< HEAD
 name = input(‘Please enter your name:’)
 surname = input(‘Please enter your surname:’)
 print(‘Hi, {} {}!’.format(name, surname))
-=======
-nickname = input(‘Please enter your nickname’)
-print(‘Hi, {}!’.format(nickname))
->>>>>>> origin/Katerina_Selevich
